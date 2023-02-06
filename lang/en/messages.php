a3df20a354cefd9eea9f9b0cc701517e4b27ae23
KLIPE_LD <klipe.ld@gmail.com>
2023-02-06 18:46:03 +0100

    fix messages

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,6 +3,33 @@
 return [
 
     'nav_survey_index' => 'Surveys',
-    'no_data_available' => 'No data available'
 
+    'no_data_available' => 'No data available',
+    'delete' => 'Delete',
+    'edit' => 'Edit',
+    'create' => 'Create',
+    'about' => 'About',
+
+    'survey_status_1' => 'Edit',
+    'survey_status_2' => 'Testing',
+    'survey_status_3' => 'Ready',
+
+    'survey_name' => 'Name of survey',
+    'survey_status' => 'Status of survey',
+    'survey_adding' => 'Adding of survey',
+    'survey_editing' => 'Editing of survey',
+
+    'question_type_1' => 'one option',
+    'question_type_2' => 'many options',
+
+    'question_title' => 'Body of question',
+    'question_type' => 'Type of question',
+    'question_position' => 'Position of question',
+    'question_adding' => 'Adding of question',
+    'question_editing' => 'Editing of  question',
+
+    'option_title' => 'Body of option',
+    'option_value' => 'Value of option',
+    'option_adding' => 'Adding of option',
+    'option_editing' => 'Editing of option',
 ];
